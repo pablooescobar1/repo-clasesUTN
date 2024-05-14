da50e24fe795ab666ee7d614ba3f9d61ddcdc982
pabloescobar <escobarpabloo315@gmail.com>
2024-05-13 23:13:51 -0300

    clase13

---
+++ clase 13/script.js
@@ -0,0 +1,89 @@
+/* funcion nativa:
+-isNaN (dato) es una funcion que recibe un dato y te devuelve boolen que indica si es o no un NaN
+-alert() es una funcion que recibe un string y lo muestra en un pop lp en la pagina
+-typeof() nos dice que tipo de dato es
+-prompt() es una funcion que recibe el mensaje t devuelve el valor ingreasado por el usuario (tipo string) o null en caso de que el usuario de al botton cancelar
+-parceInt()
+-parseFloat
+
+*/ 
+/*let variable =alert("papa") //devuelve undefined
+alert("el valor de retorno de alert es:" + variable)
+let dato = prompt("pepe")
+alert(typeof(dato))*/
+
+/* IF 
+
+let edad = prompt("ingrese su edad")
+if(edad >= 18){
+    alert("podes pasar")
+}
+else{
+    alert("no podes pasar")
+}
+
+sintaxis:
+un if no necesita de nadie un else necesita de un if para ser ejecutado
+if (condicion){
+    (bloque de codigo)
+
+}
+  else{
+
+  }
+
+let dato = prompt("di algo")
+if (dato ===null || dato === ""){
+    alert("ERROR: DATO NO VALIDO")
+}
+else{
+    alert("todo esta bien")
+}
+
+let dato = prompt("di algo")
+if(!dato){
+    alert("ERROR: DATO INVALIDO")
+}
+else{ 
+    alert("todo esta bien")
+}
+
+let puntos = prompt("ingresa tus puntos")
+
+if(puntos >= 0 && puntos <= 3000){ 
+    alert("principiante")
+}
+else if( puntos >= 3001 && puntos<= 6000){
+    alert("avanzado")
+}
+else if( puntos >= 6001 && puntos<= 8000){
+    alert("esperimentado")
+}
+*/
+
+/*
+EJERCICIO 1:
+let dato = prompt("¿tienes hijos?")
+if(hijos === "si"){
+    alert("true")
+}
+else{
+    alert(false)
+    }
+    
+   let dato = prompt("salario actual")
+   alert( 'su salario anual es de : $' + 12)
+   
+
+   let nombre = prompt("ingrese su nombre")
+
+   while(!pepe){
+    alert("ERROR: DATO NO VALIDO")
+    nombre = prompt("ingrese renuevamente el valor")
+   }
+   alert("dato correcto")
+   */
+
+
+    
+    
